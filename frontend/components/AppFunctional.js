1db913b80f8f43e02a1ad418cf6e6f5f16dd8eb1
ayseefsin <ayse.efsin@yandex.com>
2024-03-15 22:10:21 +0300

    email submit works

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 // önerilen başlangıç stateleri
 const initialMessage = "";
@@ -9,11 +10,25 @@ const initialIndex = 4; //  "B" nin bulunduğu indexi
 export default function AppFunctional(props) {
   const [index, setIndex] = useState(initialIndex);
   const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
 
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
   function getXY() {
+    const coordinates = [
+      "(1,1)",
+      "(2,1)",
+      "(3,1)",
+      "(1,2)",
+      "(2,2)",
+      "(3,2)",
+      "(1,3)",
+      "(2,3)",
+      "(3,3)",
+    ];
+    return coordinates[index];
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
   }
@@ -46,6 +61,7 @@ export default function AppFunctional(props) {
         }
         setIndex(index - 3);
         setMessage(initialMessage);
+        setSteps(steps + 1);
         break;
       case "down":
         if (index > 5) {
@@ -54,6 +70,7 @@ export default function AppFunctional(props) {
         }
         setIndex(index + 3);
         setMessage(initialMessage);
+        setSteps(steps + 1);
         break;
       case "left":
         if (index % 3 === 0) {
@@ -62,16 +79,19 @@ export default function AppFunctional(props) {
           break;
         }
         setIndex(index - 1);
+        setSteps(steps + 1);
         setMessage(initialMessage);
 
         break;
       case "right":
         if (index % 3 === 2) {
           setMessage("Sağa gidemezsiniz");
+
           break;
         }
         setIndex(index + 1);
         setMessage(initialMessage);
+        setSteps(steps + 1);
         break;
 
       default:
@@ -81,17 +101,35 @@ export default function AppFunctional(props) {
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+
+    setEmail(evt.target.value);
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    evt.preventDefault();
+    axios
+      .post("http://localhost:9000/api/result", {
+        x: Number(getXY()[1]),
+        y: Number(getXY()[3]),
+        steps: steps,
+        email: email,
+      })
+      .then(function (response) {
+        console.log(response);
+        setMessage(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setMessage(error.response.data.message);
+      });
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">Koordinatlar {getXY()}</h3>
+        <h3 id="steps">{steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
@@ -120,8 +158,14 @@ export default function AppFunctional(props) {
           reset
         </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          value={email}
+          onChange={onChange}
+          id="email"
+          type="email"
+          placeholder="email girin"
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
